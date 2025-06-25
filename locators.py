d0e71fd67faf7f6f8fff88b4bd2e27c0fbf4c4ce
TChoi <thomasjung.choi@gmail.com>
2025-06-25 01:10:58 -0500

    Correct amazon sign in link

--- locators.py
+++ locators.py
@@ -9,7 +9,7 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.maximize_window()
 
 # open the url
-driver.get('https://www.amazon.com/')
+driver.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
 
 # Locators:
 # By ID, preferred way of locating elements
